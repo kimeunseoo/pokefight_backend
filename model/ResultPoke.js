b3bda0691f1864cbc62e0d0ce36e84e5e5af74f7
eunseo <k.eunseo122@gmail.com>
2023-02-22 17:35:06 +0100

    dd

--- model/ResultPoke.js
+++ model/ResultPoke.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const pokeResultSchema = new mongoose.Schema({
-  
   name: String, 
-})
+});
 
 module.exports=mongoose.model("PokefightResult", pokeResultSchema);
