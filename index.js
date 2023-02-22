02e103d28c35132f9d62bebc7b4dbeb347e55ca8
eunseo <k.eunseo122@gmail.com>
2023-02-22 19:28:50 +0100

    kk

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const fs = require("fs");
 // const pool = require('./db');
 const port = 5001;
 app.use(cors());
+
 // const jsonData = require('./pokedex.json');
-const mongoose = require('mongoose')
+
+const mongoose = require('mongoose');
+const ResultPoke = require("./model/ResultPoke");
 require("dotenv").config({ path: ".env" });
 
 mongoose.connect(process.env.MONGO_URL,{
@@ -23,9 +26,8 @@ mongoose.connection.on("error", ()=>{
   console.log("err")
 })
 
-// app.use('/routername', game/saverouter); <- Router라는 폴더를 만들고 그 안에 파일을 따로 user.js/ or save.js 따로 설정했을때.. ?
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.send('hello pokemon')
 })
 
@@ -41,6 +43,10 @@ app.get("/pokemon", (req, res) => {
   }
 
 });
+
+// app.get('/pokemon', (req, res)=>{
+//   res.send(jsonData)
+// })
 
 app.get("/pokemon/:id", (req, res) => {
   try {
@@ -78,6 +84,7 @@ app.get("/pokemon/:id/:info", (req, res) => {
 
 });
 
+// app.use('/routername', game/saverouter); <- Router라는 폴더를 만들고 그 안에 파일을 따로 user.js/ or save.js 따로 설정했을때.. ?
 
 app.get("/pokefightResult", (req, res) => {
   try {
@@ -92,12 +99,24 @@ app.get("/pokefightResult", (req, res) => {
 
 });
 
-app.post("/pokefightresult", async (req, res) => {
-  await pokefight.create({
-    name:req.body.winner
-  });
-  res.send("game result saved to database!!");
-});
+// app.post("/pokefightresult", async (req, res) => {
+//   await pokefight.create({
+//     name:req.body.winner
+//   });
+//   res.send("game result saved to database!!");
+// });
+
+app.post("/game/pokefightresult",(req, res)=>{
+  pokefight.create({ name:req.body.winner})
+  .then(function(){
+    res.json('game result saved to database!')
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500)
+    res.json(err.message)
+  })
+})
 
 // app.get('/pokemon/:id', function (req, res, next) {
 //     res.json({msg: 'This is CORS-enabled for all origins!'})
@@ -125,4 +144,4 @@ app.listen(process.env.PORT ?? port, () => {
 });
 
 
-module.exports = app;
+// module.exports = app;
